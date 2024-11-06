ea76819d69edf692b9db6fdcd4cd92543092a133
caiomadeira <caiomadeiracontato@outlook.com>
2024-11-06 16:07:52 -0300

    update vhdl (no test)

--- BitSequence/desafio.vhd
+++ BitSequence/desafio.vhd
@@ -1,92 +1,108 @@
 -- CAIO MADEIRA
-
-
 --------------------------------------
 -- Biblioteca
 --------------------------------------
-library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.std_logic_unsigned.all;
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.std_logic_unsigned.ALL;
 
 --------------------------------------
 -- Entidade
 --------------------------------------
-entity desafio is
-    port(reset, clock, inA, inB: in std_logic;
-        found: out std_logic);
-end entity;
+ENTITY desafio IS
+    PORT (
+        reset, clock, inA, inB : IN STD_LOGIC;
+        found : OUT STD_LOGIC);
+END ENTITY;
 
 --------------------------------------
 -- Arquitetura
 --------------------------------------
-architecture behaviour of desafio is
-  type state is (S0, S1, S2, S3, S4, S5);
-  -- EA = Estado atual; PE = Proximo estado
-  signal ACTUAL_STATE, NEXT_STATE: state;
-begin          
+ARCHITECTURE behaviour OF desafio IS
+    TYPE state IS (S0, S1, S2, S3, S4, S5);
+    -- EA = Estado atual; PE = Proximo estado
+    SIGNAL ACTUAL_STATE, NEXT_STATE : state;
+BEGIN
 
-  process(clock, reset)
-  begin
-      if reset = '1' then 
-        ACTUAL_STATE <= S0;
-      elsif rising_edge(clock) then
-        ACTUAL_STATE <= NEXT_STATE;
-      end if;
-  end process;
+    PROCESS (clock, reset)
+    BEGIN
+        IF reset = '1' THEN
+            ACTUAL_STATE <= S0;
+        ELSIF rising_edge(clock) THEN
+            ACTUAL_STATE <= NEXT_STATE;
+        END IF;
+    END PROCESS;
 
-  process(ACTUAL_STATE, inA, inB)
-  begin
-    case ACTUAL_STATE is
-      -- S0 (IDLE)
-      when S0 => 
-        if inB = '1' then
-          NEXT_STATE <= S1;
-        else
-          NEXT_STATE <= S0;
-        end if;
+    PROCESS (ACTUAL_STATE, inA, inB)
+    BEGIN
+        CASE ACTUAL_STATE IS
+                -- S0 (IDLE)
+            WHEN S0 =>
+                IF inB = '1' THEN
+                    NEXT_STATE <= S1;
+                ELSE
+                    NEXT_STATE <= S0;
+                END IF;
 
-        -- S1
-        when S1 =>
-          if inA = '0' AND inB = '1' then
-            NEXT_STATE <= S2;
-          else
-            NEXT_STATE <= S0;
-          end if;
+                -- S1
+            WHEN S1 =>
+                IF inA = '0' AND inB = '1' THEN
+                    NEXT_STATE <= S2;
+                ELSE
+                    IF inA = '1' AND inB = '1' THEN
+                        NEXT_STATE <= ACTUAL_STATE;
+                    ELSIF inB = '0' THEN
+                        NEXT_STATE <= S0;
+                    END IF;
+                END IF;
 
-        -- S2
-        when S2 =>
-            if inA = '1' AND inB = '0' then
-              NEXT_STATE <= S3;
-            else
-              NEXT_STATE <= S0;
-            end if;
+                -- S2
+            WHEN S2 =>
+                IF inA = '1' AND inB = '0' THEN
+                    NEXT_STATE <= S3;
+                ELSE
+                    -- Verificar essa linha novamente mais tarde
+                    IF inA = '1' AND inB = '0' THEN
+                        NEXT_STATE <= ACTUAL_STATE;
+                    ELSE
+                        NEXT_STATE <= S0;
+                    END IF;
+                END IF;
 
-        -- S3
-        when S3 =>
-            if inA = '1' AND inB = '1' then
-              NEXT_STATE <= S4;
-            else
-              NEXT_STATE <= S0;
-            end if;
-        
-        -- S4
-        when S4 =>
-              if inA = '0' AND inB = '1' then
-                NEXT_STATE <= S5;
-              else
-                NEXT_STATE <= S0;
-              end if;
+                -- S3
+            WHEN S3 =>
+                IF inA = '1' AND inB = '1' THEN
+                    NEXT_STATE <= S4;
+                ELSE
+                    NEXT_STATE <= S0;
+                END IF;
 
-        -- S5
-        when S5 =>
-              if inA = '0' AND inB = '0' then
-                NEXT_STATE <= S0;
-              else 
-                NEXT_STATE <= S0;
-              end if;
-    end case;
-  end process;
+                -- S4
+            WHEN S4 =>
+                IF inA = '0' AND inB = '1' THEN
+                    NEXT_STATE <= S5;
+                ELSE
+                    IF inA = '0' AND inB = '1' THEN
+                        NEXT_STATE <= S2;
+                    ELSE
+                        NEXT_STATE <= S0;
+                    END IF;
+                END IF;
 
-  found <= '1' when ACTUAL_STATE = S5 else '0';
+                -- S5
+            WHEN S5 =>
+                IF inA = '0' AND inB = '0' THEN
+                    NEXT_STATE <= S0;
+                ELSE
+                    IF inA = '1' AND inB = '0' THEN
+                        NEXT_STATE <= S3;
+                    ELSIF inA = '0' AND inB = '1' THEN
+                        NEXT_STATE <= S2;
+                    END IF;
+                END IF;
+        END CASE;
+    END PROCESS;
 
-end architecture;
+    found <= '1' WHEN ACTUAL_STATE = S5 ELSE '0';
+
+END ARCHITECTURE;
